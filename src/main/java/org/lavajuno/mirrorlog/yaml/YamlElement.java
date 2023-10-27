d1cd0ad28bb85e6e204575d43ce31d74973ed14e
Juno <jvmeifert@gmail.com>
2023-10-27 11:04:20 -0400

    Move parsing into YamlElement constructor, add getters/setters

--- src/main/java/org/lavajuno/mirrorlog/yaml/YamlElement.java
+++ src/main/java/org/lavajuno/mirrorlog/yaml/YamlElement.java
@@ -5,160 +5,157 @@ import java.util.*;
 
 /**
  * YamlElement represents a single YAML element.
- * This can be either an element containing 0 or more children,
- * an element containing a value, or an element containing a list of values.
- * It is constructed with a vector of lines, and parses them by
+ * This can be either an element containing 0 or more YamlElements,
+ * an element containing a string, or an element containing a list of strings.
+ * It is constructed with a list of lines, and parses them by
  * recursively constructing new YamlElements.
  * YamlElement has two inheritors:
  *  - YamlList - A YamlElement that contains a list of values.
  *  - YamlValue - A YamlElement that contains a single value.
  * YamlList and YamlValue are the base cases for the recursive constructor.
- * They cannot have any children, and instead contain a String
- * (or a Vector of Strings).
+ * They cannot have any children, and instead contain a String (or a Vector of Strings).
+ * A YamlElement's children are stored in a red-black tree, so it is quick
+ * to access individual elements. You can also access all elements as a Collection.
  */
 @SuppressWarnings("unused")
 public class YamlElement {
     /**
-     * Regex that matches lines to ignore (blank or commented)
+     * Matches lines to ignore (blank or commented)
      */
     static final String IGNORE_RGX = "^( *#.*)|( *)$";
 
     /**
-     * Regex that matches lines containing an element or list head (ex. "ElementName: ")
+     * Matches lines containing an element or list head (ex. "ElementName: ")
      */
-    static final String ELEMENT_RGX =  "^ *[A-Za-z0-9_-]{1,64}: *$";
+    static final String ELEMENT_RGX =  "^ *[A-Za-z0-9_-]+: *$";
 
     /**
-     * Regex that matches lines containing a value (ex. "ElementName: ElementValue")
+     * Matches lines containing a value (ex. "ElementName: ElementValue")
      */
-    static final String STRING_RGX = "^ *[A-Za-z0-9_-]{1,64}: +\"?.{1,512}\"?$";
+    static final String STRING_RGX = "^ *[A-Za-z0-9_-]+: +\"?.+\"?$";
 
     /**
-     * Regex that matches lines containing a list entry (ex. "  - ListItem1")
+     * Matches lines containing a list entry (ex. "  - ListItem1")
      */
-    static final String LIST_ENTRY_RGX = "^ *- *.{1,512}$";
+    static final String LIST_ENTRY_RGX = "^ *- +.+$";
 
     /**
      * Possible matches for a line
      */
-    enum LINE_MATCHES {
+    private enum LINE_MATCHES {
         IGNORE, ELEMENT, STRING, NONE
     }
 
     /**
-     * This YAML element's key
+     * This YamlElement's key
      */
-    public final String KEY;
+    private final String KEY;
 
     /**
-     * This YAML element's children
+     * This YamlElement's contained YamlElements
      */
-    public final TreeMap<String, YamlElement> ELEMENTS;
+    private final TreeMap<String, YamlElement> ELEMENTS;
 
     /**
-     * Constructs a YAMLElement.
-     * @param key this YAMLElement's key
-     * @param elements this YAMLElement's children
+     * Constructs a YamlElement by recursively parsing lines of YAML into a structure of YamlElements.
+     * The outermost element will always be called "root", and will contain all elements defined in the input YAML.
+     * @param lines Lines of YAML to parse
+     * @throws InvalidPropertiesFormatException If YAML is invalid or the parser cannot continue
      */
-    YamlElement(String key, TreeMap<String, YamlElement> elements) {
-        this.KEY = key;
-        this.ELEMENTS = elements;
+    public YamlElement(List<String> lines) throws InvalidPropertiesFormatException {
+        this("root", lines, 0, lines.size(), 0);
     }
 
     /**
-     * Constructs a YAMLElement.
-     * @param lines Lines of YAML to parse
-     * @param begin Index to start parsing at
-     * @param end Index to stop parsing at
+     * Constructs a YamlElement with the given key and no children.
+     * Used by base-case inheritors of YamlElement.
+     * @param key this YamlElement's key
+     */
+    YamlElement(String key) {
+        this.KEY = key;
+        this.ELEMENTS = null;
+    }
+
+    /**
+     * Constructs a YamlElement.
+     * @param key The key (name) of this YamlElement
+     * @param lines Lines of YAML that we are parsing
+     * @param begin Index to start parsing this element at
+     * @param end Index to stop parsing this element at
      * @param indent The indent of the block to parse
      * @throws InvalidPropertiesFormatException If YAML is invalid or the parser cannot continue
      */
-    YamlElement(Vector<String> lines, int begin, int end, int indent) throws InvalidPropertiesFormatException {
-        this(parseKey(lines.get(begin)), parseElements(lines, begin + 1, end, indent));
-    }
-
-    /**
-     * Constructs a YAMLElement.
-     * This is the only public constructor, and recursively constructs YAMLElements
-     * with a vector of lines of YAML as input.
-     * The outermost element will always be called "root", and will contain all elements
-     * defined in the input YAML.
-     * @param lines Lines of YAML to parse
-     * @throws InvalidPropertiesFormatException If YAML is invalid or the parser cannot continue
-     */
-    public YamlElement(Vector<String> lines) throws InvalidPropertiesFormatException {
-        this("root", parseElements(lines, 0, lines.size(), 0));
-    }
-
-    /**
-     * Returns the first element with a given key found in this YamlElement's children.
-     * @param key The key to match
-     * @return The first element found with the given key (or null if none are found)
-     */
-    public YamlElement getElement(String key) {
-        return ELEMENTS.get(key);
-    }
-
-    /**
-     * Parses and constructs this YamlElement's children.
-     * @param lines Lines of YAML to parse
-     * @param begin Index to start parsing at
-     * @param end Index to stop parsing at
-     * @param indent Indent of the block to parse
-     * @return Vector of YamlElements belonging to this YamlElement
-     * @throws InvalidPropertiesFormatException If YAML is invalid or the parser cannot continue
-     */
-    private static TreeMap<String, YamlElement> parseElements(Vector<String> lines, int begin,
-                                                     int end, int indent) throws InvalidPropertiesFormatException {
-        TreeMap<String, YamlElement> my_elements = new TreeMap<>();
-        String line;
-        int line_indent;
-        LINE_MATCHES line_match;
-
-        /* Iterate over each line */
-        for (int i = begin; i < end; i++) {
-            line = lines.get(i);             /* Current line */
-            line_indent = parseIndent(line); /* How indented this line is */
-            line_match = matchLine(line);    /* This line's matched type */
-
+    YamlElement(String key, List<String> lines, int begin, int end, int indent)
+            throws InvalidPropertiesFormatException {
+        /* TODO Switch and if/else structure should be improved, right now it is not very readable */
+        this.KEY = key;
+        this.ELEMENTS = new TreeMap<>();
+        /* Iterate over each line in the range we are parsing */
+        for (int i = begin + 1; i < end; i++) {
+            String line = lines.get(i);                /* Current line */
+            int line_indent = parseIndent(line);       /* How indented this line is */
+            LINE_MATCHES line_match = matchLine(line); /* This line's matched type */
+            /* If this line is something we need to parse */
             if(line_match != LINE_MATCHES.IGNORE) {
-                /* Handle only lines in our indented block */
+                /* If this line matches our target indent */
                 if(line_indent == indent) {
+                    /* If there is a syntax error in this line, parseKey output will be ignored. */
+                    String k = parseKey(line);
                     switch(line_match) {
                         case ELEMENT:
+                            /* If the line is the head of a list */
                             if (i + 1 < end && lines.get(i + 1).matches(LIST_ENTRY_RGX)) {
-                                /* If the line is the head of a list */
-                                my_elements.put(parseKey(line), new YamlList(lines, i, end));
-                            } else {
-                                /* If the line is an element with 0 or more children */
-                                my_elements.put(parseKey(line), new YamlElement(lines, i, end, indent + 2));
+                                ELEMENTS.put(k, new YamlList(k, lines, i, end));
+                            }
+                            /* If the line is an element with 0 or more children */
+                            else {
+                                ELEMENTS.put(k, new YamlElement(k, lines, i, end, indent + 2));
                             }
                             break;
                         case STRING:
                             /* If the line is an element with just a string */
-                            my_elements.put(parseKey(line), new YamlValue(line));
+                            ELEMENTS.put(k, new YamlValue(k, line));
                             break;
                         case NONE:
-                            /* If no match was found */
-                            System.err.println("vv  Syntax error on line:  vv");
+                            /* If no match was found, we don't know what to do with the line */
+                            System.err.println("vv  YamlElement: Syntax error on line:  vv");
                             System.err.println(line);
-                            System.err.println("^^  ---------------------  ^^");
-                            throw new InvalidPropertiesFormatException("Syntax error on line " + i + ".");
+                            System.err.println("^^  ----------------------------------  ^^");
+                            System.err.println("(Line " + i + " of input.)");
+                            throw new InvalidPropertiesFormatException("Syntax error on line " + i + " of input.");
                     }
                 } else if(line_indent < indent) {
-                    /* Stop reading once we reach the end of our indented block. */
+                    /* Stop reading once we reach the end of our indented block */
                     break;
                 }
-                /* If the line's indent is greater, just skip reading it. */
+                /* If the line's indent is greater, just skip reading it */
+                /* (not sure if we're done yet at this point) */
             }
         }
-        return my_elements;
     }
 
     /**
+     * Returns this YamlElement's key
+     * @return this YamlElement's key
+     */
+    public String getKey() { return this.KEY; }
+
+    /**
+     * Returns the element with the given key in this YamlElement's contained elements.
+     * @param key The key to match
+     * @return The element with the given key (or null if none is found)
+     */
+    public YamlElement getElement(String key) { return ELEMENTS.get(key); }
+
+    /**
+     * Returns this YamlElement's contained elements.
+     * @return This YamlElement's contained elements.
+     */
+    public Collection<YamlElement> getElements() { return ELEMENTS.values(); }
+
+    /**
      * Parses a line to return the key it contains.
-     * We assume that we have checked the line to make sure it actually contains
-     * a key, otherwise this function may behave unexpectedly.
+     * If the line does not contain a key, this function may behave unexpectedly.
      * @param line The line to parse
      * @return The key contained in this line
      */
@@ -166,15 +163,14 @@ public class YamlElement {
 
     /**
      * Parses a line to return the number of spaces it is indented by.
+     * Probably not the best way to do this, but not a huge issue for now.
      * @param line The line to parse
      * @return How many spaces this line is indented by
      */
-    static int parseIndent(String line) {
-        return line.length() - line.stripLeading().length();
-    }
+    static int parseIndent(String line) { return line.length() - line.stripLeading().length(); }
 
     /**
-     * Matches a line to a category
+     * Matches a line to a predefined category
      * @param line Line to match
      * @return Category this line is matched to (can be NONE)
      */
@@ -192,12 +188,10 @@ public class YamlElement {
 
     /**
      * Alternate toString that preserves indents to output valid YAML.
-     * There is no need to call this function on the root YamlElement.
-     * Just call toString, it will take care of indentation for you.
      * @param indent Indent of this YamlElement
      * @return This YamlElement as a String
      */
-    public String toString(int indent) {
+    String toString(int indent) {
         StringBuilder sb = new StringBuilder();
         String indent_prefix = " ".repeat(indent);
         sb.append(indent_prefix).append(this.KEY).append(":\n");
--- src/main/java/org/lavajuno/mirrorlog/yaml/YamlList.java
+++ src/main/java/org/lavajuno/mirrorlog/yaml/YamlList.java
@@ -1,17 +1,18 @@
 package org.lavajuno.mirrorlog.yaml;
 
+import java.util.List;
 import java.util.Vector;
 
 /**
  * YamlList is a YamlElement that contains a list of Strings.
- * A YamlList never has children (YamlList.ELEMENTS will always be null).
+ * A YamlList never has children (YamlList.getElements() will always be null).
  */
 @SuppressWarnings("unused")
 public class YamlList extends YamlElement {
     /**
      * The contents of this YamlList
      */
-    public final Vector<String> CONTENTS;
+    private final Vector<String> CONTENTS;
 
     /**
      * Constructs a YamlList.
@@ -19,8 +20,9 @@ public class YamlList extends YamlElement {
      * @param begin Index to start parsing at
      * @param end Index to stop parsing at
      */
-    YamlList(Vector<String> lines, int begin, int end) {
-        super(YamlElement.parseKey(lines.get(begin)), null);
+    YamlList(String key, List<String> lines, int begin, int end) {
+        /* Construct superclass with only key */
+        super(key);
         /* Parse lines and populate list items */
         CONTENTS = new Vector<>();
         String line;
@@ -32,28 +34,36 @@ public class YamlList extends YamlElement {
             if(line.matches(YamlElement.LIST_ENTRY_RGX)) {
                 value = line.split("- ", 2)[1];
                 String head = value.substring(0, 1);
+                String tail = value.substring(value.length() - 1);
                 /* Trim quotes if they exist */
-                if(head.equals("\"") || head.equals("'")) {
+                if((head.equals("\"") && tail.equals("\"")) ||
+                        (head.equals("'") && tail.equals("'"))) {
                     CONTENTS.add(value.substring(1, value.length() - 1));
                 } else {
                     CONTENTS.add(value);
                 }
-            } else { /* If the line is NOT a valid list entry */
-                if(!line.matches(IGNORE_RGX)) {
-                    /* If the line is not a list entry or blank/comment,
-                       then we have reached the end of the list. */
-                    break;
-                }
+            }
+            /* If the line is NOT a valid list entry */
+            else {
+                /* If the line is not a list entry or blank/comment,
+                    then we have reached the end of the list. */
+                if(!line.matches(IGNORE_RGX)) { break; }
                 /* Skip blank/comment lines */
             }
         }
     }
 
+    /**
+     * This YamlList's contents.
+     * @return This YamlList's contents.
+     */
+    public List<String> getContents() { return CONTENTS; }
+
     @Override
     public String toString(int indent) {
         StringBuilder sb = new StringBuilder();
         String indent_prefix = " ".repeat(indent);
-        sb.append(indent_prefix).append(this.KEY).append(":").append("\n");
+        sb.append(indent_prefix).append(this.getKey()).append(":").append("\n");
         for(String i : this.CONTENTS) {
             sb.append(indent_prefix).append("- ").append(i).append("\n");
         }
